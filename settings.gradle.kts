4e1c50bb75e101b96cdcd16f8f00f3206f40ccc0
SahakyanSahak33 <sahakyansahak303@gmail.com>
2023-12-16 15:11:17 +0400

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        jcenter()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        jcenter()
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "DaggerApplication"
+include(":app")
+ 
